7041103ba6fca4ee403bc5f0e28c58c289e6b123
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2022-06-17 10:28:21 +0900

    Main

--- batch.cmd
+++ batch.cmd
@@ -1,4 +1,5 @@
 @echo off
 cd C:/Users/daewo/Desktop/Github/Django-Practice-1
+set DJANGO_SETTINGS_MODULE=config.settings.local
 ./venv/Scripts/activate
 cd C:/Users/daewo/Desktop/Github/Django-Practice-1/mysite
